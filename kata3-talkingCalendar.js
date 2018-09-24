bcd60daedf70d6a1b21cdad2cf7e0f1bd8d5c293
vonbutt <nick.von.buttlar@gmail.com>
2018-09-24 01:35:35 +0000

    kata 4

--- kata3-talkingCalendar.js
+++ kata3-talkingCalendar.js
@@ -1,63 +1,46 @@
 var talkingCalendar = function (date) {
 
-var split = date.split("/");
-var year = split[0];
-var month = split[1];
-var day = split[2];
+  // IMPORTANT: *** NOT MY WORK, TAKEN FROM PREP HELP FORUM TO STUDY ***
 
+  var arrayMonths = ["January", "February", "March", "April", "May", "June", "July", "August",
+  "September", "October", "November", "December"];
+  var result = new Date(date);
+  var year = result.getFullYear(result); // Taking the Year
+  var month = arrayMonths[(result.getMonth(date))]; // Taking the Month
+  var day = result.getDate(); // Taking the Day
 
-var newMonth = month;
-switch (newMonth) {
-  case "01":
-  newMonth = "January ";
-  break;
-  case "02":
-  newMonth = "February ";
-  break;
-  case "03":
-  newMonth = "March ";
-  break;
-  case "04":
-  newMonth = "April ";
-  break;
-  case "05":
-  newMonth = "May ";
-  break;
-  case "06":
-  newMonth = "June ";
-  break;
-  case "07":
-  newMonth = "July ";
-  break;
-  case "08":
-  newMonth = "August ";
-  break;
-  case "09":
-  newMonth = "September ";
-  break;
-  case "10":
-  newMonth = "October ";
-  break;
-  case "11":
-  newMonth = "November ";
-  break;
-  case "12":
-  newMonth = "December ";
-  break;
-  default:
-  "Not a month";
-}
+  if (day % 10 === 1 && (day/10 === 1 || 2 || 3)) {
+    switch (day){
+      case 11:
+       return month + " " + day + "th, " + year;
+      break;
+      case 0:
+       return month + "31st, " + year;
+      break;
+      default:
+       return month + " " + day + "st, " + year;
+  }
+  } else if(day % 10 === 2 && (day/10 === 1 || 2 || 3)){
+      switch (day){
+      case 12:
+       return month + " " + day + "th, " + year;
+      break;
+      default:
+       return month + " " + day + "nd, " + year;
+    }
+  } else if(day % 10 === 3 && (day/10 === 1 || 2 || 3)){
+      switch (day){
+      case 13:
+       return month + " " + day + "th, " + year;
+      break;
+      default:
+       return month + " " + day + "rd, " + year;
+    }
+  } else {
+     return month + " " + day + "th, " + year;
+    }
+  return result;
 
-
-
-var dayAtChar0 = day.charAt(0);
-var dayAtChar1 = day.charAt(1);
-
-if (dayAtChar0 === "0") {
-  return dayAtChar1;
-} else {
-  return day;
-}
 
 
 
@@ -66,6 +49,8 @@ if (dayAtChar0 === "0") {
 console.log(talkingCalendar("2017/12/02"));
 console.log(talkingCalendar("2007/11/11"));
 console.log(talkingCalendar("1987/08/24"));
+console.log(talkingCalendar("1991/01/9"));
+
 
 // OUTPUT
 
---
+++ kata4-calculateChange.js
@@ -0,0 +1,15 @@
+// *** GOALS ***
+// 1. Practice working with objects
+// 2. Practice working with loops
+
+var calculatorChange = function(total, cash) {
+
+
+
+
+
+};
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
